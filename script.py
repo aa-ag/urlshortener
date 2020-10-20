9674bd0fb9b4797055de759a2bc676c96e5f8215
aa-ag <aaron.aguerrevere@gmail.com>
2020-10-19 20:14:39 -0500

    tried different formats + long URLs

--- script.py
+++ script.py
@@ -1,9 +1,22 @@
 import pyshorteners
 
+# s = pyshorteners.Shortener()
+# print(s.available_shorteners)
+
+'''
+['adfly', 'bitly', 'chilpit', 'clckru', 'cuttly', 
+'dagd', 'gitio', 'isgd', 'nullpointer', 'osdb', 
+'owly', 'post', 'qpsru', 'shortcm', 'tinycc', 'tinyurl']
+'''
+
 url = input("Enter your URL: ")
+# https://github.com/aa-ag
+# https://3.141592653589793238462643383279502884197169399375105820974944592.eu/
+# https://iamtheproudownerofthelongestlongestlongestdomainnameinthisworld.com/
+# http://thelongestdomainnameintheworldandthensomeandthensomemoreandmore.com/
 
 shortener = pyshorteners.Shortener()
 
-shortened = shortener.tinyurl.short(url)
+shortened = shortener.osdb.short(url)
 
 print(shortened)
